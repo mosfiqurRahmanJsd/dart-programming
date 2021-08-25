c401677b86c4f813d295d70c4496887e732486fa
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2021-08-25 12:19:48 +0600

    collection list map

---
+++ collection-list-map-set.dart
@@ -0,0 +1,35 @@
+// void main() {
+//   // List
+//   List<String> names = ['Jack', 'Jill'];
+//   print(names.length);
+
+//   var names2 = [...names];
+
+//   names[1] = 'Mark';
+
+//   for (var n in names2) {
+//     print(n);
+//   }
+// }
+
+void main() {
+  // set
+  // var halogens = {'Fluorine', 'Chlorine'};
+  // for (var x in halogens) {
+  //   print(x);
+  // }
+
+  // var halogens = <String>{};
+  // Set<String> names = {};
+  // print(names.runtimeType);
+
+  // map
+  var gifts = {
+    // key: value
+    1: 'partridge',
+    2: 'turtledoves',
+    3: 'golden rings'
+  };
+
+  print(gifts[1]);
+}
